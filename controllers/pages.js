bb0f54e68624bf878e721fc11dfa34090e36bf4d
AntonioAp <antonioaparicioh@gmail.com>
2021-06-01 17:32:46 +0200

    wip home.pug

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,5 +1,10 @@
+const fetch = require('node-fetch');
+
+
 const pages = {
     home: (req, res) =>{
+        let title= req.query.t || "";//t ---> title doc from api
+        let data = await prod.getProducts(``)
 
 
     },
