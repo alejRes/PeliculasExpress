1aadefd43dcb64dce41732ffb80d5c50d4e94e17
alejRes <alejrespal@gmail.com>
2021-06-02 17:17:51 +0200

    Create routes api

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+
 const express = require('express')
-const router =require ('./controllers/index')
+const router = require ('./controllers/index')
+const routerApi = require('./controllers/routerApi')
 const app = express()
 
 
 const port = 3000
 app.use("/public", express.static("public"))//enlace de css simepre por encima de los motores de visualización
+
 app.set('view engine','pug')
 app.set('views','./views')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.use('/api', routerApi)
 app.use('/',router)
+
 app.listen(port, ()=>{
     console.log(`servidor: http://localhost:${port}`)
 })
---
+++ utils/peli.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+const peli ={
+
+    getPeli: async (url)=>{
+
+        let response = await fetch(url);
+        let peli= await response.json()
+        return peli
+
+    }
+
+}
+
+module.exports=peli;
