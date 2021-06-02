dd9a0ee22c3b81ad368106c3313e36442a6676bd
AntonioAp <antonioaparicioh@gmail.com>
2021-06-02 12:28:21 +0200

    add css file

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express()
 
 
 const port = 3000
-
+app.use("/public", express.static("public"))//enlace de css simepre por encima de los motores de visualización
 app.set('view engine','pug')
 app.set('views','./views')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 app.use('/',router)
 app.listen(port, ()=>{
