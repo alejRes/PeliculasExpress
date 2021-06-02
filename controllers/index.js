adaac42e03164c87674760dd8132c1b4b6b20a59
AntonioAp <antonioaparicioh@gmail.com>
2021-06-02 11:06:46 +0200

    pugs terminados, fetch terminado y todo funcionando

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const pages =require('./pages')
 
 router.get("/", pages.home)
-router.get("/film", pages.film)
-router.post("/film", pages.film)
+router.get("/film/:title", pages.film)
+router.post("/film", pages.postFilm)
 
 module.exports = router;
--- controllers/pages.js
+++ controllers/pages.js
@@ -1,16 +1,26 @@
 const fetch = require('node-fetch');
 
+const apikey = require('../config');
+
 
 const pages = {
     home: (req, res) =>{
-        let title= req.query.t || "";//t ---> title doc from api
-        let data = await prod.getProducts(``)
+        res.status(200).render('home');
+    },
+    film: async (req,res) =>{
+        let t= req.params.title;
+        let response = await fetch(`http://www.omdbapi.com/?apikey=${apikey.key}&t=${t}`);
+        let peli= await response.json()
+        //console.log([peli])
+        // res.send([peli][0].Title)
 
+
+        res.status(200).render('film',{peli});
 
     },
-    film: (req, res) =>{
-
-
+    postFilm: (req, res) =>{
+        let titulo = req.body.titulo;
+        res.redirect(`/film/${titulo}`)
     },
 }
 
