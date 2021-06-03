f9503af06656e51179d464fb93ab6e84754f6b7b
alejRes <alejrespal@gmail.com>
2021-06-03 16:13:48 +0200

    rutas api

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,6 +1,7 @@
 const apikey = require('../config');
 const fetch = require ('node-fetch');
 
+
 const pages = {
     home: (req, res) =>{
         res.status(200).render('home');
@@ -8,9 +9,10 @@ const pages = {
 
     film: async (req,res) =>{
 
-        //DE esta forma se ejecuta el fect cuando la ruta llama a la función film
+
+        //DE esta forma se ejecuta el fect cuando la ruta llama a la función getFilm
         let t= req.params.title;
-        let response = await fetch(`http://www.omdbapi.com/?apikey=${apikey.key}&t=${t}`);
+        let response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${t}`);
         let peli= await response.json()
         
         //console.log([peli])
